4a7a868c95536d574d52bae805acf4678be15edf
rishabh <rishabhrathoretks@gmail.com>
2020-05-14 14:35:42 +0530

    structure application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container mt-4">
+        <div className='row'>
+          <div className='col-8 container-box'>
+            <h2>Hello</h2>
+          </div>
+          <div className='col-4 container-box ml-2' style={{ maxWidth: '29.333333%' }}>
+            <h2>Buttons</h2>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
